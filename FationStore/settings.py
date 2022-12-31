dcd523694eff1a2a78e86a585cb6ca769abfdf45
Quang Phạm Hữu <ilove060902@gmail.com>
2022-12-31 21:00:52 +0700

    test 3

--- FationStore/settings.py
+++ FationStore/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = '/Users/QUANG/OneDrive/FileCode/python/FationStore'
+STATIC_ROOT = '/Users/QUANG/OneDrive/FileCode/python/FationStore/core/static'
 STATIC_URL = '/static/'
 
 # Default primary key field type
